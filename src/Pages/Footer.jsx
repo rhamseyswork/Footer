6cd19f440d62502c800ed6180adab3e774fd031f
Rhamseys Garcia <rhamseysgarcia@MacBook-Pro.local>
2024-04-18 13:07:05 -0700

    first commit

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import FooterBox from '../Components/Footer_Box';
-
+import Divider from '@mui/material/Divider';
 
 function Footer() {
   return (
     <div className='Footer'>
       <FooterBox title="About">This is a footer.</FooterBox>
+      <Divider orientation="vertical" variant="middle" style={{ backgroundColor: 'black' }} flexItem />
       <FooterBox title="Contact">Email:</FooterBox>
+      <Divider orientation="vertical" variant="middle" style={{ backgroundColor: 'black' }} flexItem />
       <FooterBox title="Example">import map</FooterBox>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
